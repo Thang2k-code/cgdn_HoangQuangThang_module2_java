ff5e4fb531e0a1fb74300e7caf0d66d58a0ff6c0
Hoàng Thắng <hoangquangthang2k@gmail.com>
2025-05-23 20:18:52 +0700

    ss5_accessModifier

--- src/ss5_accessModifier/TestCircle.java
+++ src/ss5_accessModifier/TestCircle.java
@@ -1,25 +1,13 @@
 package ss5_accessModifier;
-
 import java.util.Scanner;
-
-public class TestStudent {
-    public static void main(String[] args) {
+public class TestCircle {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-
-        student student = new student();
-
-        System.out.print("Nhập tên mới: ");
-        String newName = sc.nextLine();
-
-        System.out.print("Nhập lớp mới: ");
-        String newClass = sc.nextLine();
-
-        student.setName(newName);
-        student.setClasses(newClass);
-
-        System.out.println("Thông tin sinh viên sau khi cập nhật:");
-        System.out.println(student.getInfo());
-
+        System.out.println("Input your Radius: ");
+        double r = sc.nextDouble();
+        Circle c = new Circle(r);
+        System.out.println("Radius is: " + c.getRadius());
+        System.out.println("Area is: " + c.getArea());
         sc.close();
     }
 }
--- src/ss5_accessModifier/teststudent.java
+++ src/ss5_accessModifier/teststudent.java
@@ -1,8 +1,25 @@
 package ss5_accessModifier;
+
 import java.util.Scanner;
+
 public class teststudent {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Nhập Tên của bạn: ");
+
+        student student = new student();
+
+        System.out.print("Nhập tên mới: ");
+        String newName = sc.nextLine();
+
+        System.out.print("Nhập lớp mới: ");
+        String newClass = sc.nextLine();
+
+        student.setName(newName);
+        student.setClasses(newClass);
+
+        System.out.println("Thông tin sinh viên sau khi cập nhật:");
+        System.out.println(student.getInfo());
+
+        sc.close();
     }
 }
