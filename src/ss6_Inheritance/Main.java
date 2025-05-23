70b502fa1492e299ae355cd0d1475fe3f6fd46e1
Hoàng Thắng <hoangquangthang2k@gmail.com>
2025-05-23 21:05:08 +0700

    ss6_Inheritance

---
+++ src/ss6_Inheritance/Main.java
@@ -0,0 +1,11 @@
+package ss6_Inheritance;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(2.5, "blue");
+        System.out.println(circle);
+
+        Cylinder cylinder = new Cylinder(2.5, "blue", 5.0);
+        System.out.println(cylinder);
+    }
+}
