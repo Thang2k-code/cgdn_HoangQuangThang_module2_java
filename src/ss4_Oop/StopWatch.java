79d0702b2d8bf5a9549420bd7c79ae32117c2247
Hoàng Thắng <hoangquangthang2k@gmail.com>
2025-05-23 18:44:50 +0700

    ss4_Oop

--- src/ss4_Oop/StopWatch.java
+++ src/ss4_Oop/StopWatch.java
@@ -1,2 +1,34 @@
-package ss4_Oop;public class StopWatch {
+package ss4_Oop;
+
+public class StopWatch {
+    private long startTime;
+    private long endTime;
+    public StopWatch() {
+        this.startTime = System.currentTimeMillis();
+    }
+    public void start() {
+        this.startTime = System.currentTimeMillis();
+    }
+    public void stop() {
+        this.endTime = System.currentTimeMillis();
+    }
+    public long getElapsedTime() {
+        return this.endTime - this.startTime;
+    }
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            if (minIndex != i) {
+                int temp = arr[minIndex];
+                arr[minIndex] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
 }
